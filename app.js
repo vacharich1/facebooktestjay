1617a34278cac7fb2942ec45069d0baac041d34f
vacharich1 <jr30149_@hotmail.com>
2019-06-15 02:37:54 -0700

    Update app.js

--- app.js
+++ app.js
@@ -363,7 +363,13 @@ function receivedPostback(event) {
 
   // When a postback is called, we'll send a message back to the sender to 
   // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
+  if(payload == 'Get Started'){
+      sendTextMessage(senderID, "Thank");
+  } 
+  else
+  {
+  	sendTextMessage(senderID, "Postback called");
+  }
 }
 
 /*
